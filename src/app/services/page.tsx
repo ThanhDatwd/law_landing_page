21dad7a9783dcb3758b95017d2b7afab8ce53306
ThanhDatwd <nguyenthanhdatntd01@gmail.com>
2024-03-11 21:45:11 +0700

    update-ui

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -33,7 +33,7 @@ export default function ServicePage() {
     >
       <div
         className="relative w-full h-[80vh] bg-cover bg-center"
-        style={{ backgroundImage: "url(/assets/images/banner.jpg" }}
+        style={{ backgroundImage: "url(/assets/images/service_banner.jpeg" }}
       >
         <div className="absolute  top-0 left-[50%] text-center  translate-x-[-50%] w-[767px] px-5 h-full flex flex-col gap-8 items-center justify-center max-w-full">
           <h2 className="text-[32px] text-center lg:text-[40px] leading-[50px] text-[#fff]">
