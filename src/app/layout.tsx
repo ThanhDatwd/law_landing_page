74508c1a7d4dc87f1328a63404c9c7c9963f4e4f
ThanhDatwd <nguyenthanhdatntd01@gmail.com>
2024-03-20 17:22:27 +0700

    fix-seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,26 +8,7 @@ import { getStaticURL } from "@/utils/constants";
 import Head from "next/head";
 import { NextSeo } from "next-seo";
 
-export const metadata: Metadata = {
-  title: "Green Software",
-  description: `At Green Software, we appreciate passion, enthusiasm, creativeness and encourage every individuals to develop step by step, from good to great.`,
-  openGraph: {
-    title: "Green Software",
-    description: ` At Green Software, we appreciate passion, enthusiasm, creativeness and encourage every individuals to develop step by step, from good to great.`,
-    url: `${getStaticURL()}`,
-    siteName: "Green Software",
-    images: [
-      {
-        url: `${getStaticURL()}/assets/images/seo_logo.png`, // Must be an absolute URL
-        width: 600,
-        height: 400,
-        alt: "Logo Green Software",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
+export const metadata: Metadata = {};
 export default function RootLayout({
   children,
 }: {
@@ -35,28 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <Head>
-        <NextSeo
-          title="Green Software"
-          description=" At Green Software, we appreciate passion, enthusiasm, creativeness and encourage every individuals to develop step by step, from good to great."
-          canonical={`${getStaticURL()}`}
-          openGraph={{
-            url: `${getStaticURL()}`,
-            title: "Green Software",
-            description: ` At Green Software, we appreciate passion, enthusiasm, creativeness and encourage every individuals to develop step by step, from good to great.`,
-            images: [
-              {
-                url: `${getStaticURL()}/assets/images/seo_logo.png`,
-                width: 600,
-                height: 400,
-                alt: "Banner Green Software",
-                type: "image/png",
-              },
-            ],
-            siteName: "Green Software",
-          }}
-        />
-      </Head>
       <html lang="en">
         <body suppressHydrationWarning={true}>
           <ThemeProvider>
